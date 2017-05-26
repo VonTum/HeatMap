813fe4de7cbb8dba8277cbbe1ef3ea989fb34865
lennartVH01 <lennart.vanhirtum@gmail.com>
2017-05-26 20:57:21 +0200

    Created base directory and main file

---
+++ src/com/monolytum/heatmap/HeatMap.java
@@ -0,0 +1,15 @@
+package com.monolytum.heatmap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HeatMap extends JavaPlugin{
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
