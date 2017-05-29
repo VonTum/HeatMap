d25d2b07ea54171a5a3e535e77f111b106c9a960
lennartVH01 <lennart.vanhirtum@gmail.com>
2017-05-30 00:15:14 +0200

    Updated PlayerMovementMap

---
+++ src/com/monolytum/heatmap/Localization.java
@@ -0,0 +1,10 @@
+package com.monolytum.heatmap;
+
+import org.bukkit.configuration.Configuration;
+
+public class Localization {
+	
+	public static void reload(Configuration config){
+		
+	}
+}
--- src/com/monolytum/heatmap/PlayerMovementMap.java
+++ src/com/monolytum/heatmap/PlayerMovementMap.java
@@ -8,21 +8,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMovementMap extends AbstractHeatMap implements Listener {
-
+	
 	public PlayerMovementMap(BaseHeatMap map) {
 		super(map);
 	}
-
+	
 	@Override
 	void enable() {
-		Bukkit.getPluginManager().registerEvents(this, HeatMap.heatMap);
+		Bukkit.getPluginManager().registerEvents(this, HeatMap.plugin);
 	}
-
+	
 	@Override
 	void disable() {
 		PlayerMoveEvent.getHandlerList().unregister(this);
 	}
-
+	
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
 		Location location = e.getPlayer().getLocation();
