2d6087ab8776222e41825bd91691ebc2aea20563
lennartVH01 <lennart.vanhirtum@gmail.com>
2017-05-31 00:29:42 +0200

    Added reset clause

--- src/com/monolytum/heatmap/HeatMapAdminCommand.java
+++ src/com/monolytum/heatmap/HeatMapAdminCommand.java
@@ -1,12 +1,14 @@
 package com.monolytum.heatmap;
 
 import com.monolytum.heatmap.IO.FileWriter;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HeatMapAdminCommand implements CommandExecutor, TabCompleter {
 	private static final String[] TABCOMPLETE_LIST = {
@@ -27,6 +29,9 @@ public class HeatMapAdminCommand implements CommandExecutor, TabCompleter {
 			break;
 		case "debug":
 			FileWriter.writeDebugFile(HeatMap.plugin.playerHeatMapStore, args[1]);
+			break;
+		case "reset":
+			HeatMap.plugin.playerHeatMapStore.data = new HashMap<HeatMap2D.ChunkPosition, byte[]>();
 			break;
 		}
 		return false;
